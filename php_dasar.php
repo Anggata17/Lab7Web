efd3140ac8ea688d0856f49dc02ce185139344b3
anggathifal <anggata310@gmail.com>
2022-05-18 17:50:51 +0700

    tugas praktikum 7

---
+++ php_dasar.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <title>PHP Dasar</title>
+</head>
+
+<body>
+  <h1>Belajar PHP Dasar</h1>
+  <?php
+  echo "Hello World";
+  ?><br>
+
+  <!-- Variabel PHP -->
+  <h1>Variabel PHP</h1>
+  <?php
+  $nim = "312010419";
+  $nama = 'Angga Thifal Ananda';
+  echo "NIM : " . $nim . "<br>";
+  echo "Nama : $nama";
+  ?>
+</body>
+
+</html>
